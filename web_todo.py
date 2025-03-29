99348ca22e0a47d584fc123bed9649dca68461f0
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-03-29 15:05:57 +0000

    Implementation add, remove functions to the web_app

--- web_todo.py
+++ web_todo.py
@@ -14,7 +14,14 @@ st.subheader("This is my ToDo App")
 st.write("This app is to increase you productivity")
 
 for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f"todo_{index}")
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todo(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new To_Do...",
               on_change=add_todo, key="new_todo")
+
+st.write(st.session_state)
